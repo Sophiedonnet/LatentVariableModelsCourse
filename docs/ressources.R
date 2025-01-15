75f76553f88792553a08b2da379706a7c93679e3
Sophie Donnet <sophie.donnet@inrae.fr>
2025-01-15 10:53:57 +0100

    split

---
+++ docs/ressources.R
@@ -0,0 +1,66 @@
+---
+title: "Latent variable models for ecology and biology"
+subtitle: 
+  output: 
+  html_document:
+  toc: false
+logo: img/LogoINRAE-MIA.jpg
+number_sections: false
+highlight: tango
+includes:
+  before_body: extlogo.html
+---
+  
+  <br>
+  You will find here the material I use for my ***Latent Variables Models for ecology and biology*** class  in [Master 2  *Mathématiques pour les Sciences du Vivant*](https://sites.google.com/view/m2-msv/) of [University Paris Saclay](https://www.universite-paris-saclay.fr/en/education/master/mathematics-and-applications/m2-mathematiques-pour-les-sciences-du-vivant). 
+
+<img src="img/bee.jpeg" alt="mpe" width = "280px" align="right"> </img>
+  
+  
+  <br>
+  
+  **Practical organization**
+  
+  In 2025, the class will take place at **AgroParisTech, 22 place de l'Agronomie, Palaiseau** in room Pal-C2.1.33, on wednesdays from 14:00 to 17:00
+
+
+
+  * 08/01/2025
+  * 15/01/2025
+  * 22/01/2025
+  * 29/01/2025 
+  * 12/02/2025 
+  * 19/02/2025
+  * 05/03/2025
+
+<br>
+
+**Evaluation**
+
+The evaluation consists in a 3 hours exam  the 19th of March 2025.  We can find [here](LatentVariablesModels/examen_2024.pdf) the exam of 2024.
+
+<br>
+
+**Ressources**
+<br>
+
+
+
+<img src="img/HMM.png" alt="mpe" width = "280px" align="right"> </img>
+
+The course is based on a book that we are writing currently with [Stéphane Robin](https://scj-robin.github.io/) and [Pierre Gloaguen](https://papayoun.github.io/).  Before the book is ready, you will find  some useful  slides below. Note that I won't always stick to these documents and may change some stuff.  
+
+
+| Chapitre | Slides  | Illustration  | Ressources |
+  |:-------------|:-------:|:--------|:------:| 
+  | **Chap 1** : Latent variable models and EM |  [<i class="fa fa-download"></i>](../Slides/Chap1_PresCours/LVM_pres.pdf)| [Adelie Penguins](../IllustrationsRmd/Chap1_MixtureAdelie/Chap1_PalmerPinguins.html)| [Files](../IllustrationsRmd/Chap1_MixtureAdelie.zip) | 
+  | **Chap 2** : Explicit E step |  [<i class="fa fa-download"></i>](../Slides/Chap2_LVM_GeneralDef/chap2_LVM_Mixtures.pdf) | [Bohemia Vegetation](../IllustrationsRmd/Chap2_1_GaussianMixtureModel/Chap2_1_BohemiaVegetation.html)| [Files](../IllustrationsRmd/Chap2_1_GaussianMixtureModel.zip)| 
+  | |  | [Barents fishes](../IllustrationsRmd/Chap2_2_ZeroInflatedPoisson/Chap2_2_BarentFishesZIP.html)| [Files](../IllustrationsRmd/Chap2_2_ZeroInflatedPoisson.zip)| 
+  | **Chap 3** : Non explicit E step (HMM) |  [<i class="fa fa-download"></i>](../Slides/Chap3_LVM_NonExplicitE/chap3_LVM_HMM.pdf) | To come | 
+  | **Chap 4** : Approximated E step (Variational EM)|  [<i class="fa fa-download"></i>](../Slides/Chap4_LVM_VariationalE/Slides_SBM_LBM/slides_SBM_LBM_intro/chap4_LVM_SBM_LBM.pdf) |  [Parasitism Ecological network ](https://sophiedonnet.github.io/IntroductionToBlockModels/TP_SBM/TutorialBM.html) |  |
+  | **Chap5** : A gentle introduction to Variational probabilistic Autoencoder|  [<i class="fa fa-download"></i>](../Slides/Chap5_VarAutoEncoder/SlidesVAE/chap5_LVM_VAE.pdf) | | 
+  | **Chap 6** : Bayesien inference of Latent variable models |  [<i class="fa fa-download"></i>](../Slides/Chap6_LBM_Bayesian/chap6_LVM_bayesian_main.pdf)  | [MCMC versus VEM on Poisson mixture](https://Sophiedonnet.github.io/BayesianClassMaterial/PoissonMixtureVBversusMCMC.html) | [Files](https://sophiedonnet.github.io/BayesianClassMaterial/PoissonMixtureVB.zip)
+
+
+
+
